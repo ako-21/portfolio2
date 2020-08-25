54664dd3971bb0e9dadec5ae849aa2bbfc3e8461
ako-21 <akeemogle@gmail.com>
2020-08-25 16:41:52 -0400

    Added contact section Need to finish contact functionality so submission will come to my email

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,4 @@
 $(() => {
-
+  $('#linkedin').on('click', () => window.open('https://www.linkedin.com/in/akeemogle/', '_blank'))
+  $('#github').on('click', () => window.open('https://github.com/ako-21/', '_blank'))
 })
