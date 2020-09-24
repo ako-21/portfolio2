f4328f41847a34d6a3236376f4c7f58e131182e5
ako-21 <akeemogle@gmail.com>
2020-09-24 14:50:13 -0400

    made project screen shots clickable

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,4 +1,9 @@
 $(() => {
   $('#linkedin').on('click', () => window.open('https://www.linkedin.com/in/akeemogle/', '_blank'))
   $('#github').on('click', () => window.open('https://github.com/ako-21/', '_blank'))
+  $('#toolimg').on('click', () => window.open('https://ako-21.github.io/toolbox-client/#/', '_blank'))
+  $('#swankyimg').on('click', () => window.open('https://callback-ktz.github.io/swanky-spirits-client/#/', '_blank'))
+  $('#quickimg').on('click', () => window.open('https://ako-21.github.io/quick-sale-client/', '_blank'))
+  $('#ticimg').on('click', () => window.open('https://ako-21.github.io/tic-tac-toe/', '_blank'))
+  $('#844img').on('click', () => window.open('http://www.844atlsell.com/', '_blank'))
 })
